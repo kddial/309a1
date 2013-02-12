ef9b16ac00df505aa0619f982db59d2d0414a3da
joejian <sfjjian@gmail.com>
2013-02-12 18:36:43 -0500

    Update test2.js

--- test2.js
+++ test2.js
@@ -6,7 +6,7 @@ var stuff = [ {topic: '-1', vote: '-1', replies: "[]"} ];
 
 http.createServer(function (request, response) {
 
-  if(request.method=='POST') {
+	if(request.method=='POST') {
 		console.log('POST');
 		var body='';
 			request.on('data', function (data) {
@@ -17,49 +17,56 @@ http.createServer(function (request, response) {
 			console.log(stuff);
 		});
 		response.end();
-	}  else if ((request.url == "/") || (request.url == "/index.css")) {
-   var filePath = '.' + request.url;
-   if (filePath == './')
-       filePath = './test2.html';
+	} else {
+		if (request.url == "/get") {
+			console.log("GET");
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			response.end(JSON.stringify( {topics: stuff} ));
+		} else {
+		   var filePath = '.' + request.url;
+		   if (filePath == './')
+			   filePath = './test2.html';
 
-   var extname = path.extname(filePath);
-   var contentType = 'text/html';
-   switch (extname) {
-       case '.js':
-           contentType = 'text/javascript';
-           break;
-       case '.css':
-           contentType = 'text/css';
-           break;
-   }
+		   var extname = path.extname(filePath);
+		   var contentType = 'text/html';
+		   switch (extname) {
+			   case '.js':
+				   contentType = 'text/javascript';
+				   break;
+			   case '.css':
+				   contentType = 'text/css';
+				   break;
+		   }
 
-   path.exists(filePath, function(exists) {
+		   path.exists(filePath, function(exists) {
 
-       if (exists) {
-           fs.readFile(filePath, function(error, content) {
-               if (error) {
-                   response.writeHead(500);
-                   response.end();
-               }
-               else {
-                   response.writeHead(200, {'Content-Type': contentType });
-                   response.end(content, 'utf-8');
-               }
-           });
-       }
-       else {
-           response.writeHead(404);
-           response.end();
-       }
-   });
-   }	else //if ((request.method=='GET') & (!(request.url == "/") || (request.url == "/index.css"))) {
+			   if (exists) {
+				   fs.readFile(filePath, function(error, content) {
+					   if (error) {
+						   response.writeHead(500);
+						   response.end();
+					   }
+					   else {
+						   response.writeHead(200, {'Content-Type': contentType });
+						   response.write(content, 'utf-8');
+						   response.end();
+					   }
+				   });
+			   }
+			   else {
+				   response.writeHead(404);
+				   response.end();
+			   }
+	    });
+   	/* else //if ((request.method=='GET') & (!(request.url == "/") || (request.url == "/index.css"))) {
 		{console.log("GET");
 		response.writeHead(200, {'Content-Type': 'text/plain' });
 		var ret = "hello"
-		console.log(ret);
+		//console.log(ret);
 		response.end("hello", 'utf-8');
+	} */
+		};
 	}
-
 }).listen(31135);
 
 console.log('Server running at http://127.0.0.1:31135/');
